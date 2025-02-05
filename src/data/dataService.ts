e0dfa0976304ac9a670b982632e74b836fff9bc4
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-05 13:57:58 +0700

    feat[dataService.ts]: Added json dataService

---
+++ src/data/dataService.ts
@@ -0,0 +1,30 @@
+import { FaShippingFast, FaCreditCard, FaGift, FaUndo } from "react-icons/fa";
+
+const dataService = [
+  {
+    id: 1,
+    title: "Gratis Ongkir",
+    description: "Minimal Pembelian 100 ribu",
+    icon: FaShippingFast,
+  },
+  {
+    id: 2,
+    title: "Quick Payment",
+    description: "Pembayaran Cepat dan Aman",
+    icon: FaCreditCard,
+  },
+  {
+    id: 3,
+    title: "Gratis Hadiah",
+    description: "Syarat & Ketentuan Berlaku",
+    icon: FaGift,
+  },
+  {
+    id: 4,
+    title: "Gratis Return",
+    description: "Syarat & Ketentuan Berlaku",
+    icon: FaUndo,
+  },
+];
+
+export default dataService;
