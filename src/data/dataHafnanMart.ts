3a1fc0cdf078f2d26dca8ce7848ec75f82661c51
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-05 13:57:34 +0700

    feat[dataHafnanMart.ts]: Added json dataHafnanMart

---
+++ src/data/dataHafnanMart.ts
@@ -0,0 +1,238 @@
+const dataHafnanMart = [
+  {
+    id: 1,
+    name: "Bihun Cap Tanam Jagung 320g",
+    price: "Rp 8.125",
+    category: "Bihun & Soun",
+    image: "/image/products/hafnanmart/Bihun Cap Tanam Jagung 320g.png",
+    marketplaces: [
+      {
+        name: "Tokopedia",
+        logo: "/image/marketplace/tokopedia.png",
+        url: "https://www.tokopedia.com/hafnan99/bihun-cap-tanam-jagung-320g-1730786404406298454",
+      },
+      {
+        name: "Shopee",
+        logo: "/image/marketplace/shopee.png",
+        url: "https://shopee.co.id/example/bihun-cap-tanam-jagung-320g",
+      },
+      {
+        name: "WhatsApp",
+        logo: "/image/marketplace/whatsapp.png",
+        url: "https://wa.me/6281380525404?text=Saya%20ingin%20membeli%20Bihun%20Cap%20Tanam%20Jagung%20320g",
+      },
+    ],
+    detail: [
+      {
+        condition: "Baru",
+        minOrder: "1",
+        storefront: "Bihun",
+        description:
+          "Bihun Cap Tanam Jagung 320g adalah pilihan tepat untuk Anda yang menginginkan hidangan praktis.",
+      },
+    ],
+    specification: [
+      {
+        storage: "Suhu Ruangan",
+        timeStorage: "Lainnya",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Teh Celup Sariwangi Asli 25s",
+    price: "Rp 7.657",
+    category: "Teh",
+    image: "/image/products/hafnanmart/Teh Celup Sariwangi Asli 25s.png",
+    marketplaces: [
+      {
+        name: "Tokopedia",
+        logo: "/image/marketplace/tokopedia.png",
+        url: "https://www.tokopedia.com/hafnan99/teh-celup-sariwangi-asli-25s-1730786178120845142",
+      },
+      {
+        name: "Shopee",
+        logo: "/image/marketplace/shopee.png",
+        url: "https://shopee.co.id/example/bihun-cap-tanam-jagung-320g",
+      },
+      {
+        name: "WhatsApp",
+        logo: "/image/marketplace/whatsapp.png",
+        url: "https://wa.me/6281380525404?text=Saya%20ingin%20membeli%20Teh%20Celup%20Sariwangi%20Asli%2025s",
+      },
+    ],
+    detail: [
+      {
+        condition: "Baru",
+        minOrder: "1",
+        storefront: "Teh",
+        description:
+          "Teh Celup Sariwangi Asli 25s adalah pilihan ideal bagi Anda yang mencari teh berkualitas tinggi dengan rasa yang autentik dan menyegarkan.",
+      },
+    ],
+    specification: [
+      {
+        storage: "Suhu Ruangan",
+        timeStorage: "Lainnya",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Minyak Urut Cap Lang GPU 30ml",
+    price: "Rp 14.500",
+    category: "Minyak Urut",
+    image: "/image/products/hafnanmart/Minyak Urut Cap Lang GPU 30ml.png",
+    marketplaces: [
+      {
+        name: "Tokopedia",
+        logo: "/image/marketplace/tokopedia.png",
+        url: "https://www.tokopedia.com/hafnan99/minyak-urut-cap-lang-gpu-30ml-1730786509111723862",
+      },
+      {
+        name: "Shopee",
+        logo: "/image/marketplace/shopee.png",
+        url: "https://shopee.co.id/example/bihun-cap-tanam-jagung-320g",
+      },
+      {
+        name: "WhatsApp",
+        logo: "/image/marketplace/whatsapp.png",
+        url: "https://wa.me/6281380525404?text=Saya%20ingin%20membeli%20Minyak%20Urut%20Cap%20Lang%20GPU%2030ml",
+      },
+    ],
+    detail: [
+      {
+        condition: "Baru",
+        minOrder: "1",
+        storefront: "Minyak Urut",
+        description:
+          "Minyak Urut Cap Lang GPU 30ml adalah solusi alami yang dirancang khusus untuk membantu meredakan nyeri otot dan sendi, serta memberikan kenyamanan pada tubuh setelah aktivitas fisik.",
+      },
+    ],
+    specification: [
+      {
+        storage: "Suhu Ruangan",
+        timeStorage: "Lainnya",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Minyak Urut Cap Lang GPU 60ml",
+    price: "Rp 18.500",
+    category: "Minyak Urut",
+    image: "/image/products/hafnanmart/Minyak Urut Cap Lang GPU 60ml.png",
+    marketplaces: [
+      {
+        name: "Tokopedia",
+        logo: "/image/marketplace/tokopedia.png",
+        url: "https://www.tokopedia.com/hafnan99/minyak-urut-cap-lang-gpu-60ml-1730786448726067030",
+      },
+      {
+        name: "Shopee",
+        logo: "/image/marketplace/shopee.png",
+        url: "https://shopee.co.id/example/bihun-cap-tanam-jagung-320g",
+      },
+      {
+        name: "WhatsApp",
+        logo: "/image/marketplace/whatsapp.png",
+        url: "https://wa.me/6281380525404?text=Saya%20ingin%20membeli%20Minyak%20Urut%20Cap%20Lang%20GPU%2060ml",
+      },
+    ],
+    detail: [
+      {
+        condition: "Baru",
+        minOrder: "1",
+        storefront: "Minyak Urut",
+        description:
+          "Minyak Urut Cap Lang GPU 60ml adalah solusi alami yang dirancang khusus untuk membantu meredakan nyeri otot dan sendi, serta memberikan kenyamanan pada tubuh setelah aktivitas fisik.",
+      },
+    ],
+    specification: [
+      {
+        storage: "Suhu Ruangan",
+        timeStorage: "Lainnya",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Minyak Goreng Tropical 2 Liter",
+    price: "Rp 42.356",
+    category: "Minyak Goreng",
+    image: "/image/products/hafnanmart/Minyak Goreng Tropical 2 Liter.png",
+    marketplaces: [
+      {
+        name: "Tokopedia",
+        logo: "/image/marketplace/tokopedia.png",
+        url: "https://www.tokopedia.com/hafnan99/minyak-goreng-tropical-2-liter",
+      },
+      {
+        name: "Shopee",
+        logo: "/image/marketplace/shopee.png",
+        url: "https://shopee.co.id/example/bihun-cap-tanam-jagung-320g",
+      },
+      {
+        name: "WhatsApp",
+        logo: "/image/marketplace/whatsapp.png",
+        url: "https://wa.me/6281380525404?text=Saya%20ingin%20membeli%20Minyak%20Goreng%20Tropical%202%20Liter",
+      },
+    ],
+    detail: [
+      {
+        condition: "Baru",
+        minOrder: "1",
+        storefront: "Minyak Goreng",
+        description:
+          "Tropical adalah minyak goreng berkualitas yang diproses dari kelapa sawit pilihan dengan penyaringan terbaik.",
+      },
+    ],
+    specification: [
+      {
+        storage: "Suhu Ruangan",
+        timeStorage: "Lainnya",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Gula Pasir Gulaku Kuning 1kg",
+    price: "Rp 20.625",
+    category: "Gula Pasir",
+    image: "/image/products/hafnanmart/Gula Pasir Gulaku Kuning 1kg.png",
+    marketplaces: [
+      {
+        name: "Tokopedia",
+        logo: "/image/marketplace/tokopedia.png",
+        url: "https://www.tokopedia.com/hafnan99/gula-pasir-gulaku-kuning-1kg",
+      },
+      {
+        name: "Shopee",
+        logo: "/image/marketplace/shopee.png",
+        url: "https://shopee.co.id/example/bihun-cap-tanam-jagung-320g",
+      },
+      {
+        name: "WhatsApp",
+        logo: "/image/marketplace/whatsapp.png",
+        url: "https://wa.me/6281380525404?text=Saya%20ingin%20membeli%20Gula%20Pasir%20Gulaku%20Kuning%201Kg",
+      },
+    ],
+    detail: [
+      {
+        condition: "Baru",
+        minOrder: "1",
+        storefront: "Gula Pasir",
+        description:
+          "Gulaku Gula Tebu adalah gula pasir yang diproduksi dari tetes tebu alami dengan kandungan kadar molasses yang lebih tinggi untuk menciptakan aroma caramel yang alami.",
+      },
+    ],
+    specification: [
+      {
+        storage: "Suhu Ruangan",
+        timeStorage: "Lainnya",
+      },
+    ],
+  },
+];
+
+export default dataHafnanMart;
