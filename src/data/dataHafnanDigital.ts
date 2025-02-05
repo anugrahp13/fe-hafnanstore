09ff8dbc6d7df438944e13a5af7144ea4920d5da
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-05 13:57:09 +0700

    feat[dataHafnanDigital.ts]: Added json dataHafnanDigital

---
+++ src/data/dataHafnanDigital.ts
@@ -0,0 +1,38 @@
+const dataHafnanDigital = [
+  {
+    id: 1,
+    name: "12 - CVR - MD KEMURAHAN VS KEMAHALAN",
+    image:
+      "/image/products/hafnandigital/12 - CVR - MD KEMURAHAN VS KEMAHALAN.png",
+    videoUrl: "1i5n-IFL8q9u0EOmraa9pHOUTjVrix_Iz",
+    landingPage: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=1",
+    checkout: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=3",
+  },
+  {
+    id: 2,
+    name: "33 - CVR - TESTI VERLY (HOUSEWIFE)",
+    image:
+      "/image/products/hafnandigital/33 - CVR - TESTI VERLY (HOUSEWIFE).png",
+    videoUrl: "1qdumtsJEiEgEdrQM3fZEhSaW6LIQpFOs",
+    landingPage: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=1",
+    checkout: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=3",
+  },
+  {
+    id: 3,
+    name: "CVR - TESTI BUDI PARDEDE",
+    image: "/image/products/hafnandigital/CVR - TESTI BUDI PARDEDE.png",
+    videoUrl: "15VdJSWWdfA5G3RwsYlJHjSY9xOujj03i",
+    landingPage: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=1",
+    checkout: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=3",
+  },
+  {
+    id: 4,
+    name: "CVR - TESTI ZAKY",
+    image: "/image/products/hafnandigital/CVR - TESTI ZAKY.png",
+    videoUrl: "1EnYUECMEXUII7hnhbvWm8lXSvE3eQHMA",
+    landingPage: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=1",
+    checkout: "https://member.mahirdigital.id/lp?aff=anug9ed0&i=3",
+  },
+];
+
+export default dataHafnanDigital;
