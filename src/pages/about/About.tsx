4a871d8048974d52ed9cd61d44ef65a600e062d6
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-09 13:10:02 +0700

    feat[*]: Import picture in folder products

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,10 +1,8 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { IoMail } from "react-icons/io5";
-import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export const About = () => {
+  const [activeTab, setActiveTab] = useState("store");
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -12,78 +10,123 @@ export const About = () => {
     <>
       <HelmetProvider>
         <Helmet>
-          <title>About Me - Portfolio Anugrah Prastyo</title>
+          <title>Products - Hafnan Store</title>
         </Helmet>
       </HelmetProvider>
       <section className="my-52 dark:bg-dark">
         <div className="container mx-auto px-4 lg:max-w-7xl flex items-center justify-center">
-          <div className="max-w-3xl text-center">
-            <div className="text-sm text-primary dark:text-primary grid gap-3">
-              <span className="font-bold tracking-[.30em] uppercase text-base">
-                About
-              </span>
-              <span className="font-bold text-dark mb-10 text-4xl lg:text-5xl dark:text-white">
-                More About Me
-              </span>
+          <div className="text-center grid gap-10">
+            <div>
+              <img
+                src="/image/banner/banner hafnan store.png"
+                alt="Banner Hafnan Store"
+                className="w-full pt-[3.8rem] h-[41rem] object-cover"
+              />
             </div>
-            <div className="grid gap-12 mt-8">
-              <div className="text-left grid gap-4 mb-5">
-                <div className="text-2xl font-bold">📞 Connect with Me</div>
-                <div className="text-left grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5">
-                  <Link
-                    to="mailto:anugrahprastyo13@gmail.com"
-                    className="gap-3 p-4 shadow-md bg-white hover:dark:shadow-slate-700/50 dark:bg-slate-800 rounded-xl cursor-pointer active:scale-95 active:shadow-sm transition-all hover:shadow-lg"
+            <div className="flex flex-col md:flex-row gap-10">
+              {/* Sidebar */}
+              <div className="w-full md:w-1/4">
+                <h2 className="font-bold text-lg uppercase">
+                  Tentang Hafnan Store
+                </h2>
+                <div className="mt-4 flex flex-col gap-2">
+                  <button
+                    className={`p-3 text-center font-semibold ${
+                      activeTab === "store"
+                        ? "bg-blue-600 text-white"
+                        : "bg-gray-200 text-black"
+                    }`}
+                    onClick={() => setActiveTab("store")}
                   >
-                    <div className="text-center flex justify-center items-center gap-3 mb-3">
-                      <IoMail className="w-[1.10rem] h-[1.10rem]" />
-                      <h3 className="text-xl font-bold">Email</h3>
-                    </div>
-                    <p className="font-medium text-sm text-center line-clamp-2 text-slate-700 dark:text-slate-300">
-                      anugrahprastyo13@gmail.com
-                    </p>
-                  </Link>
-                  <Link
-                    to="https://www.linkedin.com/in/anugrah-prastyo/"
-                    className="gap-3 p-4 shadow-md bg-white hover:dark:shadow-slate-700/50 dark:bg-slate-800 rounded-xl cursor-pointer active:scale-95 active:shadow-sm transition-all hover:shadow-lg"
+                    Hafnan Store
+                  </button>
+                  <button
+                    className={`p-3 text-center font-semibold ${
+                      activeTab === "mart"
+                        ? "bg-blue-600 text-white"
+                        : "bg-gray-200 text-black"
+                    }`}
+                    onClick={() => setActiveTab("mart")}
                   >
-                    <div className="text-center flex justify-center items-center gap-3 mb-3">
-                      <FaLinkedin className="w-5 h-5" />
-                      <h3 className="text-xl font-bold">Linkedin</h3>
-                    </div>
-                    <p className="font-medium text-sm text-center line-clamp-2 text-slate-700 dark:text-slate-300">
-                      Anugrah Prastyo
-                    </p>
-                  </Link>
-                  <Link
-                    to="https://github.com/anugrahp13"
-                    className="gap-3 p-4 shadow-md bg-white hover:dark:shadow-slate-700/50 dark:bg-slate-800 rounded-xl cursor-pointer active:scale-95 active:shadow-sm transition-all hover:shadow-lg"
+                    Hafnan Mart
+                  </button>
+                  <button
+                    className={`p-3 text-center font-semibold ${
+                      activeTab === "digital"
+                        ? "bg-blue-600 text-white"
+                        : "bg-gray-200 text-black"
+                    }`}
+                    onClick={() => setActiveTab("digital")}
                   >
-                    <div className="text-center flex justify-center items-center gap-3 mb-3">
-                      <FaGithub className="w-5 h-5" />
-                      <h3 className="text-xl font-bold">GitHub</h3>
-                    </div>
-                    <p className="font-medium text-sm text-center line-clamp-2 text-slate-700 dark:text-slate-300">
-                      @anugrahp13
-                    </p>
-                  </Link>
+                    Hafnan Digital
+                  </button>
                 </div>
               </div>
-              <div className="text-left grid gap-4 mb-5">
-                <div className="text-2xl font-bold">⚡ Tech Stack</div>
-              </div>
-              <div className="text-left grid gap-4 mb-5">
-                <div className="text-2xl font-bold">💼 Work Experiences</div>
-              </div>
-              <div className="text-left grid gap-4 mb-5">
-                <div className="text-2xl font-bold">🎓 Educations</div>
-              </div>
-              <div className="text-left grid gap-4 mb-5">
-                <div className="text-2xl font-bold">🏆 Award</div>
-              </div>
-              <div className="text-left grid gap-4">
-                <div className="text-2xl font-bold">
-                  📜 Certifications, Courses or Bootcamp
-                </div>
+
+              {/* Content */}
+              <div className="w-full md:w-3/4">
+                {activeTab === "store" ? (
+                  <div>
+                    <h2 className="text-2xl font-bold">Tentang Hafnan Store</h2>
+                    <p className="mt-4 text-gray-700 dark:text-white text-left leading-7">
+                      <span className="font-bold">Hafnan Store</span> adalah
+                      aplikasi berbasis website yang menghadirkan berbagai
+                      keunggulan dalam satu platform, dengan mengintegrasikan
+                      layanan seperti Hafnan Mart dan Hafnan Digital. Hafnan
+                      Store siap menjadi pilihan utama untuk memenuhi segala
+                      kebutuhan Anda.
+                    </p>
+                  </div>
+                ) : activeTab === "mart" ? (
+                  <div>
+                    <h2 className="text-2xl font-bold">Tentang Hafnan Mart</h2>
+                    <p className="mt-4 text-gray-700 dark:text-white text-left leading-7">
+                      <span className="font-bold">Hafnan Mart</span> adalah
+                      solusi belanja kebutuhan pokok yang praktis dan
+                      terpercaya, hadir dalam layanan online dan offline. Kami
+                      menyediakan berbagai produk berkualitas, mulai dari
+                      <span className="font-semibold">
+                        {" "}
+                        tepung, minyak goreng, bahan kue, susu kental manis,
+                        penyedap rasa, kopi, gula pasir, kecap santan, parfum,
+                        balsem, mie kering, mie instan, teh, bihun
+                      </span>{" "}
+                      hingga berbagai kebutuhan rumah tangga lainnya. Dengan
+                      komitmen untuk memberikan harga terjangkau dan produk
+                      berkualitas, Hafnan Mart memastikan setiap pelanggan
+                      mendapatkan pengalaman belanja yang nyaman dan memuaskan.
+                      Belanja di toko kami secara langsung atau manfaatkan
+                      layanan online untuk kemudahan berbelanja dari rumah
+                      dengan sistem pengiriman cepat dan aman. Nikmati
+                      kemudahan, kepastian kualitas, dan pelayanan terbaik hanya
+                      di{" "}
+                      <span className="font-bold">
+                        Hafnan Mart – Solusi Praktis Untuk Kebutuhan Harian Anda
+                      </span>
+                      .
+                    </p>
+                  </div>
+                ) : (
+                  <div>
+                    <h2 className="text-2xl font-bold">
+                      Tentang Hafnan Digital
+                    </h2>
+                    <p className="mt-4 text-gray-700 dark:text-white text-left leading-7">
+                      <span className="font-bold">Hafnan Digital</span> adalah
+                      solusi lengkap bagi Anda yang mencari berbagai kebutuhan
+                      digital dengan kualitas terbaik. Kami menghadirkan beragam
+                      produk dan layanan inovatif, mulai dari undangan digital
+                      yang elegan, tema website profesional, hingga kursus
+                      digital marketing yang dirancang untuk meningkatkan
+                      keterampilan dan kesuksesan bisnis Anda di era digital.
+                      Dengan komitmen untuk memberikan kemudahan dan kepuasan
+                      pelanggan, Hafnan Digital selalu menghadirkan produk
+                      berkualitas, harga terjangkau, dan layanan yang responsif.
+                      Bergabunglah dengan kami untuk menjelajahi dunia digital
+                      yang lebih luas dan berkembang bersama Hafnan Digital!
+                    </p>
+                  </div>
+                )}
               </div>
             </div>
           </div>
--- src/pages/product/types/HafnanDigital.type.ts
+++
@@ -1,8 +0,0 @@
-export interface HafnanDigitalProps {
-  id: number;
-  name: string;
-  image: string;
-  videoUrl: string;
-  landingPage: string;
-  checkout: string;
-}
