9d18142eaeb55ec1f791bd9d0ccf4a7a3b3ff06e
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-09 12:56:01 +0700

    feat[Footer.tsx]: Remove Link Social Media

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
@@ -7,29 +5,10 @@ export const Footer = () => {
       <footer className="bg-dark py-6 dark:bg-slate-800">
         <div className="container">
           <div className="w-full pt-4">
-            {/* <div className="flex items-center justify-center gap-4 mb-5">
-              <Link
-                to="/"
-                className="w-9 h-9 rounded-full flex justify-center items-center border border-slate-300 text-slate-300 hover:border-primary hover:text-white hover:bg-primary dark:border-white hover:dark:border-primary"
-              >
-                <FaInstagram className="w-5 h-5" />
-              </Link>
-              <Link
-                to="/"
-                className="w-9 h-9 rounded-full flex justify-center items-center border border-slate-300 text-slate-300 hover:border-primary hover:text-white hover:bg-primary dark:border-white hover:dark:border-primary"
-              >
-                <FaFacebookF className="w-5 h-5" />
-              </Link>
-              <Link
-                to="/"
-                className="w-9 h-9 rounded-full flex justify-center items-center border border-slate-300 text-slate-300 hover:border-primary hover:text-white hover:bg-primary dark:border-white hover:dark:border-primary"
-              >
-                <FaTiktok className="w-5 h-5" />
-              </Link>
-            </div> */}
+            
             <p className="font-medium text-sm text-slate-300 text-center">
               © 2025{" "}
-              <span className="font-bold text-primary dark:text-white">
+              <span className="font-bold text-white">
                 Hafnan Store
               </span>
               . All rights reserved.
