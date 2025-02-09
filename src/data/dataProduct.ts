85f1060c3232f1875fcb294d8418edfd314b15b5
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-09 12:57:44 +0700

    Feat[DataProduct.ts]: Create file dataProduct

---
+++ src/data/dataProduct.ts
@@ -0,0 +1,14 @@
+const dataProduct = [
+  {
+    id: 1,
+    name: "Hafnan Mart",
+    image: "image/products/hafnanmart/hafnanmart.png",
+  },
+  {
+    id: 2,
+    name: "Hafnan Digital",
+    image: "image/products/hafnanmart/hafnanmart.png",
+  },
+];
+
+export default dataProduct;
