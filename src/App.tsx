60fb03d3f7956362133c56426ac4e695c3d25b09
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-09 13:09:11 +0700

    feat[App.tsx]: Create function ProductsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import { Layout } from "./layouts/Layout";
 import { NotFound } from "./components/NotFound";
 import { Home } from "./pages/home/Home";
 import { Contact } from "./pages/contact/Contact";
 import { About } from "./pages/about/About";
+import { Products } from "./pages/products/Products";
+import { HafnanMarts } from "./pages/hafnanmart/HafnanMart";
+import dataHafnanMart from "./data/dataHafnanMart";
+import dataHafnanDigital from "./data/dataHafnanDigital";
+import { HafnanDigitals } from "./pages/hafnandigital/Hafnandigital";
 
+function ProductsPage() {
+  const { name } = useParams();
+
+  if (name === "hafnan-mart") {
+    return <HafnanMarts hafnanmarts={dataHafnanMart} />;
+  } else if (name === "hafnan-digital") {
+    return <HafnanDigitals hafnandigitals={dataHafnanDigital} />;
+  } else {
+    return <NotFound />;
+  }
+}
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<About />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/products/:name" element={<ProductsPage />} />
       </Routes>
     </Layout>
   );
