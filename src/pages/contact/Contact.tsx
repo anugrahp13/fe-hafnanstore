4f1be9af731f64f4cd22facd27665e48d99a430d
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-12 10:38:20 +0700

    feat[Contact.tsx]: Added interface ContactType and import SectionLeft

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { IoMail } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { ContactsProps } from "./types/Contact.type";
+import { SectionLeft } from "./SectionLeft";
+import dataContact from "../../data/dataContact";
 
-export const Contact = () => {
+interface ContactsType {
+  contacts: ContactsProps[];
+}
+export const Contact: React.FC<ContactsType> = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -11,43 +15,45 @@ export const Contact = () => {
     <>
       <HelmetProvider>
         <Helmet>
-          <title>Contact Me - Portfolio Anugrah Prastyo</title>
+          <title>Contact - Hafnan Store</title>
         </Helmet>
       </HelmetProvider>
       <section className="my-52 dark:bg-dark">
-        <div className="container mx-auto px-4 lg:max-w-7xl flex items-center justify-center">
+        <div className="container mx-auto px-4 lg:max-w-7xl flex flex-col items-center justify-center min-h-screen">
           <div className="max-w-4xl text-center grid gap-10">
-            <div className="text-sm text-primary dark:text-primary grid gap-3">
-              <span className="font-bold tracking-[.30em] uppercase text-base">
-                Contact
-              </span>
-              <span className="font-bold text-dark text-4xl mt-3 lg:text-5xl dark:text-white">
-                Let's Work <span className="text-primary">Together</span>
-              </span>
-            </div>
-            <div className="text-3xl font-medium text-dark lg:text-5xl grid gap-4 dark:text-white">
-              <p>Do you have a project to work on?</p>
-              <p>Want to cooperate or</p>
-              <p>
-                Need{" "}
-                <span className="text-primary font-semibold">
-                  Web Development
-                </span>{" "}
-                services?
-              </p>
-            </div>
-            <div
-              className="flex justify-center items-center text-sm aos-init aos-animate"
-              data-aos="fade-up"
-              data-aos-duration="1000"
-            >
-              <Link
-                to="mailto:anugrahprastyo13@gmail.com"
-                className="font-semibold rounded-lg flex justify-center items-center px-6 py-2 border bg-primary border-primary text-white hover:border-primary hover:text-white hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out gap-2"
-              >
-                Contact via Email
-                <IoMail className="w-[1.10rem] h-[1.10rem]" />
-              </Link>
+            <div className="bg-gray-200 p-6 flex flex-col md:flex-row gap-10 border rounded-lg items-center">
+              <SectionLeft contacts={dataContact} />
+              <div className="w-full md:w-3/4">
+                <div className="grid gap-5">
+                  <h2 className="text-2xl font-bold">Contact</h2>
+                  <div className="text-gray-700 dark:text-white text-left leading-7">
+                    <ul
+                      role="list"
+                      className="list-disc mb-4 marker:text-sky-400 text-left md:text-left"
+                    >
+                      <li>
+                        <span className="font-bold">Tokopedia :</span> Hafnan99
+                      </li>
+                      <li>
+                        <span className="font-bold">Shopee :</span> Hafnanmart
+                      </li>
+                    </ul>
+                    <p>
+                      "Dapatkan harga terbaik dengan berkunjung langsung ke toko
+                      offline kami! Klik{" "}
+                      <span className="font-bold">Lihat Peta</span> untuk
+                      menemukan lokasi kami. Jika ada pertanyaan tentang produk,
+                      jangan ragu untuk klik{" "}
+                      <span className="font-bold">Hubungi</span> dan tim kami
+                      siap membantu!" 🚀
+                    </p>
+                    <p className="text-red-500">
+                      NB: Barang yang di jual di marketplace belum semua di
+                      upload dan Harga di Marketplace sudah plus biaya Admin.
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
