6b62634a86bfb3e1d3483404b3ee3ea8a0798fc4
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-12 10:26:29 +0700

    feat[Button.tsx]: Remove border dashed

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -14,7 +14,7 @@ export const Button: React.FC<ButtonProps> = ({
     "font-semibold rounded-lg flex justify-center items-center px-6 py-2 gap-2";
   const variants = {
     primary:
-      "border-2 border-slate-800 dark:border-white border-dashed dark:border-white hover:dark:border-primary text-slate-800 hover:border-primary hover:text-white dark:text-white hover:bg-primary",
+      "border-2 border-slate-800 dark:border-white dark:border-white hover:dark:border-primary text-slate-800 hover:border-primary hover:text-white dark:text-white hover:bg-primary",
     secondary:
       "border-2 bg-primary border-primary text-white hover:border-primary hover:text-white hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out",
     tertiary:
