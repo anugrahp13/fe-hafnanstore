65ea857973cf96ee8a164d1b21b4055e1068ba4e
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-16 22:37:35 +0700

    feat[Contact.type.ts]: Added property email in ContactProps

--- src/pages/contact/types/Contact.type.ts
+++ src/pages/contact/types/Contact.type.ts
@@ -1,16 +1,19 @@
 export interface ContactsProps {
-    id: number,
-    address: {
-        name: string,
-        location: string,
-    }[],
-    whatsapps: {
-        name: string,
-        url: string,
-    }[];
-    operasionals: {
-        day: string,
-        time: string,
-    }[];
-  }
-  
+  id: number;
+  emails: {
+    name: string;
+    url: string;
+  }[];
+  address: {
+    name: string;
+    location: string;
+  }[];
+  whatsapps: {
+    name: string;
+    url: string;
+  }[];
+  operasionals: {
+    day: string;
+    time: string;
+  }[];
+}
