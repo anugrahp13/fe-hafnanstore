b2a85bb52146082b9a90f2af3996a8e71adec377
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-16 22:32:24 +0700

    feat[dataFooter.ts]: Create file datajson dataFooter

---
+++ src/data/dataFooter.ts
@@ -0,0 +1,37 @@
+const dataFooter = [
+  {
+    id: 1,
+    information: "Informasi",
+    aboutme: "Tentang Kami",
+    available: "Kami Tersedia di",
+    subscription: "Berlangganan",
+    iteminformations: [
+      {
+        name: "Syarat & Ketentuan",
+        url: "/syarat-pengembalian",
+      },
+      {
+        name: "FAQ",
+        url: "/faq",
+      },
+      {
+        name: "Testimoni",
+        url: "/testimoni",
+      },
+    ],
+    marketplaces: [
+      {
+        name: "Tokopedia",
+        logo: "/image/marketplace/tokopedia.png",
+        url: "https://www.tokopedia.com/hafnan99",
+      },
+      {
+        name: "Shopee",
+        logo: "/image/marketplace/shopee.png",
+        url: "https://shopee.co.id/hafnanmart",
+      },
+    ],
+  },
+];
+
+export default dataFooter;
