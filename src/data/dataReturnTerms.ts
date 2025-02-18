f93529b94a5f91dfc9a5e40f4356906f74adf764
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-19 06:47:16 +0700

    feat[dataReturnTerms.ts]: Create dataJson dataReturnTerms

---
+++ src/data/dataReturnTerms.ts
@@ -0,0 +1,83 @@
+const dataReturnTerms = [
+  {
+    id: 1,
+    question: "Pengiriman",
+    answer: [
+      {
+        id: 1,
+        answer:
+          "Pengiriman dilakukan melalui jasa ekspedisi yang tersedia, termasuk JNE, J&T, GoSend, dan Grab.",
+      },
+      {
+        id: 2,
+        answer:
+          "Waktu pengiriman dapat bervariasi tergantung lokasi dan jenis layanan yang dipilih.",
+      },
+      {
+        id: 3,
+        answer:
+          "Jika terjadi keterlambatan yang disebabkan oleh pihak ekspedisi, pelanggan dapat menghubungi layanan pelanggan.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Pengembalian & Refund",
+    answer: [
+      {
+        id: 1,
+        answer:
+          "Pengembalian barang hanya berlaku untuk produk yang rusak akibat proses pengiriman.",
+      },
+      {
+        id: 2,
+        answer:
+          "Pelanggan wajib menyertakan video unboxing tanpa edit atau potongan sebagai bukti klaim.",
+      },
+      {
+        id: 3,
+        answer:
+          "Pengajuan pengembalian harus dilakukan dalam 1x24 jam setelah barang diterima.",
+      },
+      {
+        id: 4,
+        answer:
+          "Proses pengembalian dana akan dilakukan dalam 1x24 jam setelah pengajuan disetujui.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Pembatalan Pesanan",
+    answer: [
+      {
+        id: 1,
+        answer:
+          "Pesanan dapat dibatalkan jika pembayaran tidak dilakukan dalam batas waktu yang ditentukan.",
+      },
+      {
+        id: 2,
+        answer:
+          "Jika pesanan sudah dalam proses pengiriman, pembatalan tidak dapat dilakukan.",
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Layanan Pelanggan",
+    answer: [
+      {
+        id: 1,
+        answer:
+          "Pelanggan dapat menghubungi layanan pelanggan melalui menu Contact untuk pertanyaan atau keluhan terkait pesanan.",
+      },
+      {
+        id: 2,
+        answer:
+          "Anda bisa menghubungi kami untuk sekedar menanyakan produk terbaik atau ingin membandingkan produk A dan B.",
+      },
+    ],
+  },
+];
+
+export default dataReturnTerms;
