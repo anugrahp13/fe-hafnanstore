49e2b39342f339bf1707900ef12314d9f7883bb7
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-19 06:46:34 +0700

    feat[dataFaq.ts]: Create dataJson dataFaq

---
+++ src/data/dataFaq.ts
@@ -0,0 +1,45 @@
+const dataFaq = [
+  {
+    id: 1,
+    question: "Kapan waktu pengiriman?",
+    answer: [
+      {
+        name: "JNE, J&T",
+        answer:
+          "Pesanan yang masuk diatas pukul 17:00 WIB akan dikirim pada hari yang sama tergantung kondisi. Jika kondisi tidak memungkinkan maka pesanan akan dikirim hari berikutnya.",
+      },
+      {
+        name: "Gosend Instant",
+        answer:
+          "Pesanan yang masuk diatas pukul 14:00 WIB akan dikirim pada hari berikutnya.",
+      },
+      {
+        name: "Gosend Sameday, Grab Instant & Grab Sameday",
+        answer:
+          "Pesanan yang masuk diatas pukul 16:00 WIB akan dikirim pada hari berikutnya.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Apakah ada toko fisik yang bisa saya kunjungi?",
+    answer: "Ya, Anda bisa mengunjungi toko fisik kami. Anda bisa cek di bagian menu Contact kemudian klik Lihat Peta."
+  },
+  {
+    id: 3,
+    question: "Apakah bisa melakukan pengembalian barang?",
+    answer: "Ya, Pengembalian barang bisa dilakukan hanya untuk barang yang rusak akibat proses pengiriman, untuk lebih lengkapnya anda bisa masuk ke menu Syarat & Ketentuan."
+  },
+  {
+    id: 4,
+    question: "Apakah proses pengembalian barang harus menyertakan video unboxing?",
+    answer: "Ya, Pengembalian barang dapat proses jika menyertakan video unboxing tanpa di CUT, untuk lebih lengkapnya anda bisa masuk ke menu Syarat & Ketentuan."
+  },
+  {
+    id: 5,
+    question: "Berapa lama proses pengembalian dana?",
+    answer: "Proses pengembalian dana akan dilakukan 1x24 jam setelah solusi pada bagian komplain pesanan disetujui."
+  },
+];
+
+export default dataFaq;
