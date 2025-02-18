e71287ecf96a3ebde44ebd1efdbfff9e51b96e56
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-19 06:48:48 +0700

    feat[Cart.tsx]: Create page Cart for Return Terms

---
+++ src/pages/return/Cart.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+
+import { ReturnTermsProps } from "./types/ReturnTerms.type";
+
+interface ReturnTermsType {
+  returnterms: ReturnTermsProps[];
+}
+export const Cart: React.FC<ReturnTermsType> = ({ returnterms }) => {
+  return (
+    <>
+      <div className="space-y-6">
+        {returnterms.map((term) => (
+          <div key={term.id} className="border-b pb-4">
+            <h2 className="font-bold text-xl">{term.question}</h2>
+            <div className="mt-2 text-gray-600">
+              {Array.isArray(term.answer) ? (
+                <ul className="list-disc list-inside space-y-2">
+                  {term.answer.map((subItem) => (
+                    <li key={subItem.id} className="text-base">
+                      {subItem.answer}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p className="text-sm">{term.answer}</p>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
