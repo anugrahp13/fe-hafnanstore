fb7a88a2eff76e2231e4c2a7d7cad7950e4518b2
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-23 13:48:27 +0700

    feat[Faq.tsx]: Added background slate 800 when system dark

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -22,7 +22,7 @@ export const Faq: React.FC<FaqsType> = () => {
       <section className="my-52 dark:bg-dark">
         <div className="container mx-auto px-4 lg:max-w-7xl flex flex-col items-center justify-center">
           <div className="lg:w-2/3 max-w-4xl text-center grid gap-10">
-            <div className="bg-gray-200 p-6 flex flex-col md:flex-row gap-10 border rounded-lg items-center">
+            <div className="bg-gray-200 dark:bg-slate-800 p-6 flex flex-col md:flex-row gap-10 border rounded-lg items-center">
               <div className="w-full">
                 <div className="grid gap-5">
                   <h2 className="text-2xl font-bold">
