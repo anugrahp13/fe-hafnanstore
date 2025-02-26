6a802f11d4ae79e090053f2092399a6e2127ca28
anugrahp13 <anugrahprastyo13@gmail.com>
2025-02-26 19:48:26 +0700

    style[Contact.tsx]: Added order

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -23,7 +23,7 @@ export const Contact: React.FC<ContactsType> = () => {
           <div className="max-w-4xl text-center grid gap-10">
             <div className="bg-gray-200 dark:bg-slate-800 p-6 flex flex-col md:flex-row gap-10 border rounded-lg items-center">
               <SectionLeft contacts={dataContact} />
-              <div className="w-full md:w-3/4">
+              <div className="w-full md:w-3/4 order-1 md:order-2">
                 <div className="grid gap-5">
                   <h2 className="text-2xl font-bold">Contact</h2>
                   <div className="text-gray-700 dark:text-white text-left leading-7 space-y-4">
@@ -42,8 +42,9 @@ export const Contact: React.FC<ContactsType> = () => {
                         <span className="font-bold">Lihat Peta</span> untuk
                         menemukan lokasi kami, bila ada pertanyaan tentang
                         produk, jangan ragu untuk klik{" "}
-                        <span className="font-bold">Hubungi</span>, jika anda ingin berbisnis dengan kami jangan ragu untuk klik Email dan tim kami
-                        siap membantu!" 🚀
+                        <span className="font-bold">Hubungi</span>, jika anda
+                        ingin berbisnis dengan kami jangan ragu untuk klik Email
+                        dan tim kami siap membantu!" 🚀
                       </p>
                       <p className="text-red-500">
                         NB: Barang yang di jual di marketplace belum semua di
