c22a7bf28af31dc05431fe0f2cc20ae006745733
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-11 21:10:18 +0700

    feat[dataNexasite.ts]: create file dataNexasite

--- src/data/dataNexasite.ts
+++ src/data/dataNexasite.ts
@@ -1,9 +1,33 @@
 const dataNexasite = [
   {
     id: 1,
-    name: "VIDEO AFFILIATE PRODUK TERLARIS",
+    name: "Simple Landing Page Portfolio",
+    author: "agrptio",
+    category: "Landing Page",
     image: "/image/products/hafnandigital/VIDEO AFFILIATE PRODUK TERLARIS.png",
-    url: "https://lynk.id/hafnandigital/rxq7791",
+    description:
+      " is an online platform that aims to introduce and promote various cultures from various regions in Indonesia, known as the Archipelago. This website provides information related to culture in Indonesia such as traditional dance, traditional houses, typical cuisine and other cultural categories.",
+    role: "Frontend",
+    url: "https://lynk.id/hafnandigital/0M4lGbY",
+    price: 100000,
+    sale: 10,
+    features: [
+      "Has a tutorial search.",
+      "Has a login feature.",
+      "Include pagination in each category.",
+      "It has bright and dark modes.",
+      "The UI is visually appealing and simple to use.",
+      "The website is responsive on all devices.",
+    ],
+    nameTechStack: ["HTML", "CSS", "JavaScript", "TailwindCSS", "Figma", "Laravel"],
+    imageTechStack: [
+      "/image/skills/html5.svg",
+      "/image/skills/css3.svg",
+      "/image/skills/javascript.svg",
+      "/image/skills/tailwindcss.svg",
+      "/image/skills/figma.svg",
+      "/image/skills/laravel.svg",
+    ],
   },
 ];
 
