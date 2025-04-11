dad366e3064ed0423138fa2348ae308bcf25b786
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-11 21:19:49 +0700

    refactor[Nexasite.type.ts]: adde author, category, description, role, price, sale,features, nameTechStack, and imageTechStack

--- src/pages/home/types/Nexasite.type.ts
+++ src/pages/home/types/Nexasite.type.ts
@@ -1,6 +1,15 @@
 export interface NexasitesProps {
   id: number;
   name: string;
+  author: string;
+  category: string;
   image: string;
+  description: string;
+  role: string;
   url: string;
+  price: number;
+  sale: number;
+  features: string[];
+  nameTechStack: string[];
+  imageTechStack: string[];
 }
