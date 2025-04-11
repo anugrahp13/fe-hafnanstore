15c7f372e13b2ce8b130bb3402dcb818d3e608d0
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-11 13:33:19 +0700

    feat[dataNexasite.ts]: add data Nexasite

---
+++ src/data/dataNexasite.ts
@@ -0,0 +1,10 @@
+const dataNexasite = [
+  {
+    id: 1,
+    name: "VIDEO AFFILIATE PRODUK TERLARIS",
+    image: "/image/products/hafnandigital/VIDEO AFFILIATE PRODUK TERLARIS.png",
+    url: "https://lynk.id/hafnandigital/rxq7791",
+  },
+];
+
+export default dataNexasite;
