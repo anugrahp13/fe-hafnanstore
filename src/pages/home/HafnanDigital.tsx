d464120d4c6c2bb025418076b88283827a95cacb
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-11 21:16:49 +0700

    refactor[HafnanDigital.tsx]: import icon LoaderCircle, ShoppingBag in pages HafnanDigital from lucide-react

--- src/pages/home/HafnanDigital.tsx
+++ src/pages/home/HafnanDigital.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { Button } from "../../components/elements/Button";
 import { HafnanDigitalsProps } from "./types/HafnanDigital.type";
-import { MdOutlineShoppingBag } from "react-icons/md";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { LoaderCircle, ShoppingBag } from "lucide-react";
 
 interface HafnanDigitalsType {
   hafnandigitals: HafnanDigitalsProps[];
@@ -24,7 +23,7 @@ export const HafnanDigitals: React.FC<HafnanDigitalsType> = ({
   if (isLoading) {
     return (
       <div className="col-span-full flex flex-col items-center justify-center min-h-[400px] text-gray-500 text-center">
-        <AiOutlineLoading3Quarters className="text-6xl animate-spin text-blue-500" />
+        <LoaderCircle className="text-6xl animate-spin text-blue-500" />
         <p className="mt-4">Produk sedang dimuat...</p>
       </div>
     );
@@ -65,7 +64,7 @@ export const HafnanDigitals: React.FC<HafnanDigitalsType> = ({
                 variant="tertiary"
                 text="Beli Sekarang"
                 className="w-full lg:w-fit px-6 py-2"
-                icon={MdOutlineShoppingBag}
+                icon={ShoppingBag}
                 iconClassName="w-[1.10rem] h-[1.10rem]"
               />
             </div>
