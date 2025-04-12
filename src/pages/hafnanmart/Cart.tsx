e5d8afc02efbf4dfe2656b3487b6cabd88c173ca
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-12 12:22:19 +0700

    refactor[Cart.tsx]: changed style for image in cart pages hafnanmart

--- src/pages/hafnanmart/Cart.tsx
+++ src/pages/hafnanmart/Cart.tsx
@@ -67,23 +67,26 @@ export const Cart: React.FC<CartHafnanMartsType> = ({
             key={hafnanmart.id}
           >
             <div className="mb-3 inline-block">
-              <picture>
-                <img
-                  src={hafnanmart.image || "/image/avatar/404.png"} // Jika kosong, gunakan default
-                  alt={`Thumbnails - ${hafnanmart.name}`}
-                  className="rounded-xl object-cover w-full brightness-90 dark:brightness-100 transition-transform hover:scale-110"
-                  onError={(e) =>
-                    (e.currentTarget.src = "/image/avatar/404.png")
-                  }
-                />
-              </picture>
+              <div className="relative w-full bg-gray-200 dark:bg-gray-700 flex items-center justify-center rounded-xl overflow-hidden">
+                <picture>
+                  <img
+                    src={hafnanmart.image || "/image/avatar/404.png"} // Jika kosong, gunakan default
+                    alt={`Thumbnails - ${hafnanmart.name}`}
+                    data-size="auto"
+                    className="lazyload w-full rounded-xl object-cover max-w-full brightness-90 dark:brightness-100 lazyloaded transition-transform hover:scale-110"
+                    onError={(e) =>
+                      (e.currentTarget.src = "/image/avatar/404.png")
+                    }
+                  />
+                </picture>
+              </div>
             </div>
             <div className="text-base lg:text-sm font-semibold text-primary dark:text-slate-200 hover:dark:text-white flex justify-between items-center">
               <span>{hafnanmart.category}</span>
             </div>
-            <h3 className="text-xl lg:text-lg font-bold tracking-tight line-clamp-2 mb-3 min-h-[3rem]">
+            <div className="text-xl lg:text-lg font-bold tracking-tight line-clamp-2 mb-3 min-h-[3rem]">
               <span>{hafnanmart.name}</span>
-            </h3>
+            </div>
             <div className="flex flex-wrap justify-center items-center text-sm gap-4">
               {hafnanmart.marketplaces.map((marketplace) => (
                 <a
