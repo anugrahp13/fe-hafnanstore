a4bbc85d05c5126a9de728ec27e61fe400d7b236
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-13 17:58:19 +0700

    feat[ChooseProduct.tsx]: create pages ChooseProduct in detailproduct

---
+++ src/pages/detailproduct/ChooseProduct.tsx
@@ -0,0 +1,22 @@
+import { NexasitesProps } from "../../types/Nexasite.type";
+
+interface DetailProductProps {
+  nexasites: NexasitesProps;
+}
+export const ChooseProduct = ({ nexasites }: DetailProductProps) => {
+  return (
+    <>
+      <div className="grid gap-2 mt-4">
+        <div className="text-2xl font-bold">Mengapa Memilih Template Ini?</div>
+        <div className="leading-relaxed text-base space-y-2">
+          {nexasites.chooses.map((choose, index) => (
+            <li key={index} className="text-slate-800 dark:text-white">
+              <span className="font-bold">{choose.boldText}</span>
+              {choose.normalText}
+            </li>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
