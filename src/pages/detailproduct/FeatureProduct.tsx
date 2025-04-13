dd4db95f45d061d0d8a4ade46159cf9750d479d0
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-13 18:00:30 +0700

    feat[FeatureProduct.tsx]: create pages FeatureProduct in detailproduct

---
+++ src/pages/detailproduct/FeatureProduct.tsx
@@ -0,0 +1,22 @@
+import { NexasitesProps } from "../../types/Nexasite.type";
+
+interface DetailProductProps {
+  nexasites: NexasitesProps;
+}
+export const FeatureProduct = ({ nexasites }: DetailProductProps) => {
+  return (
+    <>
+      <div className="grid gap-2 mt-4">
+        <div className="text-2xl font-bold">Keunggulan</div>
+        <div className="leading-relaxed text-base space-y-2">
+          {nexasites.features.map((feature, index) => (
+            <li key={index} className="text-slate-800 dark:text-white">
+              <span className="font-bold">{feature.boldText}</span>
+              {feature.normalText}
+            </li>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
