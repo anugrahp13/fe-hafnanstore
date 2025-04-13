6740008719f00f29e11a45687614b58265effc63
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-13 17:58:03 +0700

    feat[AboutProduct.tsx]: create pages AboutProduct in detailproduct

---
+++ src/pages/detailproduct/AboutProduct.tsx
@@ -0,0 +1,23 @@
+import { NexasitesProps } from "../../types/Nexasite.type";
+
+interface DetailProductProps {
+  nexasites: NexasitesProps;
+}
+export const AboutProduct = ({ nexasites }: DetailProductProps) => {
+  return (
+    <>
+      <div className="text-2xl font-bold">Tentang</div>
+      <div className="leading-relaxed text-base space-y-2">
+        <span className="font-bold">{nexasites.category}</span>{" "}
+        <span className="text-slate-800 dark:text-white">
+          {nexasites.description[0]}
+        </span>
+        {nexasites.description.slice(1).map((description, index) => (
+          <p key={index} className="text-slate-800 dark:text-white">
+            {description}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+};
