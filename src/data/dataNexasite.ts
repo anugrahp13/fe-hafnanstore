deb8d14e12fb80f8c91e8c5b4f885427d8694145
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-13 17:36:23 +0700

    refactor[dataNexasite.ts]: changed value name, description, and added features, compatibles, and chooses

--- src/data/dataNexasite.ts
+++ src/data/dataNexasite.ts
@@ -1,24 +1,18 @@
 const dataNexasite = [
   {
     id: 1,
-    name: "Simple Landing Page Portfolio",
+    name: "Simple Portfolio",
     author: "agrptio",
     category: "Landing Page",
     image: "/image/products/nexasite/simple-portfolio.png",
-    description:
-      " is an online platform that aims to introduce and promote various cultures from various regions in Indonesia, known as the Archipelago. This website provides information related to culture in Indonesia such as traditional dance, traditional houses, typical cuisine and other cultural categories.",
+    description: [
+      "Merupakan solusi sempurna untuk profesional portfolio yang ingin menampilkan karya dengan cara yang stylish, simple, namun informatif, meningkatkan personal branding sekaligus menjadi alat marketing yang efektif.",
+      "Template ini dirancang untuk developer, desainer, dan profesional kreatif yang ingin tampil clean, modren, dan profesional. Dibangun dengan desain minimalis namun powerful, template ini membantu Anda membuat kesan pertama yang kuat di mata klien atau recruiter.",
+    ],
     role: "Frontend",
     url: "https://lynk.id/hafnandigital/0M4lGbY",
     price: 100000,
     sale: 10,
-    features: [
-      "Has a tutorial search.",
-      "Has a login feature.",
-      "Include pagination in each category.",
-      "It has bright and dark modes.",
-      "The UI is visually appealing and simple to use.",
-      "The website is responsive on all devices.",
-    ],
     nameTechStack: [
       "HTML",
       "CSS",
@@ -26,6 +20,72 @@ const dataNexasite = [
       "TailwindCSS",
       "Figma",
       "Laravel",
+    ],
+    features: [
+      {
+        boldText: "Desain Responsif",
+        normalText:
+          " – Tampil sempurna di semua perangkat (desktop, tablet, & mobile).",
+      },
+      {
+        boldText: "Layout Terstruktur",
+        normalText:
+          " – Informasi tersusun rapi, mulai dari profil, skill, proyek, hingga pengalaman kerja.",
+      },
+      {
+        boldText: "Visualisasi Skill yang Jelas",
+        normalText:
+          " – Progress bar untuk menampilkan tingkat keahlian (HTML, CSS, JavaScript, dll).",
+      },
+      {
+        boldText: "Portofolio Menarik",
+        normalText:
+          " – Display proyek dengan thumbnail dan progress penyelesaian.",
+      },
+      {
+        boldText: "Sertifikasi & Pengalaman",
+        normalText:
+          " – Bagian khusus untuk meningkatkan kredibilitas profesional Anda.",
+      },
+      {
+        boldText: "Mudah Dikustomisasi",
+        normalText:
+          " – Ganti teks, warna, dan gambar dengan cepat sesuai kebutuhan.",
+      },
+    ],
+    compatibles: [
+      {
+        boldText: "Freelancer",
+        normalText: " yang ingin memamerkan karya secara profesional.",
+      },
+      {
+        boldText: "Job Seeker",
+        normalText: " yang membutuhkan CV digital yang menarik.",
+      },
+      {
+        boldText: "Mahasiswa/New Grads",
+        normalText: " yang ingin membangun portofolio pertama.",
+      },
+      {
+        boldText: "Professional",
+        normalText: " yang ingin memperbarui tampilan online mereka.",
+      },
+    ],
+    chooses: [
+      {
+        boldText: "Tidak Ribet",
+        normalText: " – Cukup isi data Anda, langsung siap digunakan.",
+      },
+      {
+        boldText: "Fokus pada Konten",
+        normalText:
+          " – Desain yang tidak mengganggu, tetapi memperkuat pesan Anda.",
+      },
+      {
+        boldText: "Hemat Waktu",
+        normalText:
+          " – Tidak perlu mulai dari nol, tinggal personalisasi dan deploy.",
+      },
     ],
   },
 ];
