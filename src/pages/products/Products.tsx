5d0d5e0d632534ce7b791ca500a5425674cfdf35
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-13 18:02:51 +0700

    refactor[Products.tsx]: changed margin horizontal

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -14,7 +14,7 @@ export const Products = () => {
           <title>Products - Hafnan Store</title>
         </Helmet>
       </HelmetProvider>
-      <section className="my-52 dark:bg-dark">
+      <section className="my-32 dark:bg-dark">
         <div className="container mx-auto px-4 lg:max-w-7xl flex items-center justify-center">
           <div className="text-center grid gap-10">
             <div className="text-sm text-primary dark:text-primary grid gap-3">
