aefe941909123e0fa78e485111e0ed464daad2c9
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-13 17:58:36 +0700

    feat[CompotibleProduct.tsx]: create pages CompotibleProduct in detailproduct

---
+++ src/pages/detailproduct/CompotibleProduct.tsx
@@ -0,0 +1,22 @@
+import { NexasitesProps } from "../../types/Nexasite.type";
+
+interface DetailProductProps {
+  nexasites: NexasitesProps;
+}
+export const CompotibleProduct = ({ nexasites }: DetailProductProps) => {
+  return (
+    <>
+      <div className="grid gap-2 mt-4">
+        <div className="text-2xl font-bold">Template Cocok Untuk Siapa?</div>
+        <div className="leading-relaxed text-base space-y-2">
+          {nexasites.compatibles.map((compatible, index) => (
+            <li key={index} className="text-slate-800 dark:text-white">
+              <span className="font-bold">{compatible.boldText}</span>
+              {compatible.normalText}
+            </li>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
