d5d1ea712e12e6eb7b371a62ac60770eb9415e6f
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-14 21:29:54 +0700

    refactor[FeatureProduct.tsx]: remove property leading-relaxed

--- src/pages/detailproduct/FeatureProduct.tsx
+++ src/pages/detailproduct/FeatureProduct.tsx
@@ -8,7 +8,7 @@ export const FeatureProduct = ({ nexasites }: DetailProductProps) => {
     <>
       <div className="grid gap-2 mt-4">
         <div className="text-2xl font-bold">Keunggulan</div>
-        <div className="leading-relaxed text-base space-y-2">
+        <div className="text-base space-y-2">
           {nexasites.features.map((feature, index) => (
             <li key={index} className="text-slate-800 dark:text-white">
               <span className="font-bold">{feature.boldText}</span>
