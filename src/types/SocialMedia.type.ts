84ec11cb3679eca427e7332b261ecd7eb7995382
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-16 19:44:39 +0700

    feat[SocialMedia.type.ts]: create component SocialMediaProps

---
+++ src/types/SocialMedia.type.ts
@@ -0,0 +1,7 @@
+import { LucideIcon } from "lucide-react";
+
+export interface SocialMediaProps {
+  to: string;
+  icon?: LucideIcon; // Menggunakan LucideIcon sebagai tipe
+  iconClassName?: string;
+}
