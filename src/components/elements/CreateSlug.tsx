e3366b18314fbe4afe3a72b9887a46e7eb277db4
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-16 19:45:59 +0700

    refactor[CreateSlug.tsx]: update replace character, space, and multi dash

--- src/components/elements/CreateSlug.tsx
+++ src/components/elements/CreateSlug.tsx
@@ -1,7 +1,17 @@
 // utils.js
-export const createSlug = (name: any) => {
-  return name
+// export const createSlug = (name: any) => {
+//   return name
+//     .toLowerCase()
+//     .replace(/\s+/g, "-") // Mengganti spasi dengan "-"
+//     .replace(/[^\w-]+/g, ""); // Menghapus karakter selain huruf, angka, dan "-"
+// };
+
+// utils/createSlug.ts
+export const createSlug = (text: string) => {
+  return text
     .toLowerCase()
-    .replace(/\s+/g, "-") // Mengganti spasi dengan "-"
-    .replace(/[^\w-]+/g, ""); // Menghapus karakter selain huruf, angka, dan "-"
+    .replace(/[^\w\s-]/g, "") // Hapus karakter khusus
+    .replace(/\s+/g, "-") // Ganti spasi dengan dash
+    .replace(/--+/g, "-") // Hilangkan multiple dash
+    .trim();
 };
