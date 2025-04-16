3e1a53706f4d531b8a90da194c447bf7d58c2670
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-16 19:56:14 +0700

    refactor[Cart.tsx]: added state management for ReturnTerm store

--- src/pages/return/Cart.tsx
+++ src/pages/return/Cart.tsx
@@ -1,33 +1,30 @@
-/* eslint-disable react/prop-types */
+// components/Cart.tsx
+import { useReturnTermsStore } from "../../stores/useReturnTermStore";
 
-import { ReturnTermsProps } from "../../types/ReturnTerms.type";
 
-interface ReturnTermsType {
-  returnterms: ReturnTermsProps[];
-}
-export const Cart: React.FC<ReturnTermsType> = ({ returnterms }) => {
+export const Cart = () => {
+  const { returnTerms } = useReturnTermsStore();
+
   return (
-    <>
-      <div className="space-y-6">
-        {returnterms.map((term) => (
-          <div key={term.id} className="pb-4">
-            <h2 className="font-bold text-xl">{term.question}</h2>
-            <div className="mt-2 text-gray-600 dark:text-white">
-              {Array.isArray(term.answer) ? (
-                <ul className="list-disc list-inside space-y-2">
-                  {term.answer.map((subItem) => (
-                    <li key={subItem.id} className="text-base">
-                      {subItem.answer}
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="text-sm">{term.answer}</p>
-              )}
-            </div>
+    <div className="space-y-6">
+      {returnTerms.map((term) => (
+        <div key={term.id} className="pb-4">
+          <h2 className="font-bold text-xl">{term.question}</h2>
+          <div className="mt-2 text-gray-600 dark:text-white">
+            {Array.isArray(term.answer) ? (
+              <ul className="list-disc list-inside space-y-2">
+                {term.answer.map((subItem) => (
+                  <li key={subItem.id} className="text-base">
+                    {subItem.answer}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-sm">{term.answer}</p>
+            )}
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+    </div>
   );
 };
