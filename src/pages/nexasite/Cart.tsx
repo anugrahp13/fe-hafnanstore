16cd914ef23a83f996a792d0be1164cce168b325
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-16 19:55:47 +0700

    refactor[Cart.tsx]: added state management for Nexasite store

--- src/pages/nexasite/Cart.tsx
+++ src/pages/nexasite/Cart.tsx
@@ -1,23 +1,20 @@
+// components/Cart.tsx
 import { useEffect, useState } from "react";
-import { Button } from "../../components/elements/Button";
-import { NexasitesProps } from "../../types/Nexasite.type";
-import dataNexasite from "../../data/dataNexasite";
 import { Link } from "react-router-dom";
 import { LoaderCircle, Video } from "lucide-react";
+import { NexasitesProps } from "../../types/Nexasite.type";
 import { createSlug } from "../../components/elements/CreateSlug";
+import { Button } from "../../components/elements/Button";
 
-interface CartNexasitesType {
-  nexasites: NexasitesProps[];
-  searchQuery?: string; // Query pencarian (opsional)
+
+interface CartProps {
+  products: NexasitesProps[];
 }
 
-export const Cart: React.FC<CartNexasitesType> = ({
-  nexasites = [],
-  searchQuery = "",
-}) => {
+export const Cart = ({ products }: CartProps) => {
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    // Simulasikan delay loading
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 200);
@@ -33,21 +30,15 @@ export const Cart: React.FC<CartNexasitesType> = ({
       </div>
     );
   }
-  // Jika hafnanmarts bukan array, tampilkan pesan error
-  if (!Array.isArray(nexasites)) {
-    console.error("Error: nexasites bukan array!", nexasites);
+
+  if (!Array.isArray(products)) {
+    console.error("Error: products bukan array!", products);
     return (
       <p className="text-red-500">Terjadi kesalahan dalam memuat produk.</p>
     );
   }
 
-  // Filter produk berdasarkan pencarian
-  const filteredProducts = dataNexasite.filter((product) =>
-    product.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  // Jika tidak ada hasil pencarian, tampilkan gambar "Produk Tidak Ditemukan"
-  if (filteredProducts.length === 0) {
+  if (products.length === 0) {
     return (
       <div className="col-span-full flex flex-col items-center justify-center min-h-[400px] text-gray-500 text-center">
         <img
@@ -59,58 +50,57 @@ export const Cart: React.FC<CartNexasitesType> = ({
       </div>
     );
   }
+
   return (
     <>
-      {filteredProducts.map((nexasites) => {
-        return (
-          <div
-            className="shadow-md p-6 rounded-2xl bg-white dark:bg-slate-800 hover:shadow-lg dark:hover:outline dark:hover:outline-slate-600 dark:hover:outline-1 grid gap-1"
-            key={nexasites.id}
+      {products.map((product) => (
+        <div
+          className="shadow-md p-6 rounded-2xl bg-white dark:bg-slate-800 hover:shadow-lg dark:hover:outline dark:hover:outline-slate-600 dark:hover:outline-1 grid gap-1"
+          key={product.id}
+        >
+          <Link
+            to={`/products/nexasite/${createSlug(product.name)}`}
+            className="mb-3 inline-block"
           >
-            <Link
-              to={`/products/nexasite/${createSlug(nexasites.name)}`}
-              className="mb-3 inline-block"
-            >
-              <div className="relative w-full bg-gray-200 dark:bg-gray-700 flex items-center justify-center rounded-xl overflow-hidden">
-                <picture>
-                  <img
-                    src={nexasites.image}
-                    data-size="auto"
-                    alt={nexasites.name}
-                    className="lazyload w-full rounded-xl object-cover max-w-full brightness-90 dark:brightness-100 lazyloaded transition-transform hover:scale-110"
-                  />
-                </picture>
-              </div>
-            </Link>
-            <div className="tracking-tight line-clamp-2 mb-3 min-h-[3rem] overflow-hidden">
-              <p className="text-base font-bold truncate">{nexasites.name}</p>
-              <p className="text-xs truncate">
-                by{" "}
-                <Link to="#" className="font-semibold">
-                  {nexasites.author}
-                </Link>
-              </p>
+            <div className="relative w-full bg-gray-200 dark:bg-gray-700 flex items-center justify-center rounded-xl overflow-hidden">
+              <picture>
+                <img
+                  src={product.image}
+                  data-size="auto"
+                  alt={product.name}
+                  className="lazyload w-full rounded-xl object-cover max-w-full brightness-90 dark:brightness-100 transition-transform hover:scale-110"
+                />
+              </picture>
             </div>
-            <div className="flex flex-wrap justify-between items-center text-sm gap-4">
-              <div className="tracking-tight line-clamp-2 min-h-[3rem]">
-                <p className="text-lg font-bold">
-                  Rp. {""}
-                  {nexasites.price.toLocaleString("id-ID")}
-                </p>
-                <p className="text-xs">{nexasites.sale} Terjual</p>
-              </div>
-              <Button
-                href={nexasites.url}
-                variant="primary"
-                text="Lihat Demo"
-                className="w-full lg:w-fit"
-                icon={Video}
-                iconClassName="w-4 h-4"
-              />
-            </div>
+          </Link>
+          <div className="tracking-tight line-clamp-2 mb-3 min-h-[3rem] overflow-hidden">
+            <p className="text-base font-bold truncate">{product.name}</p>
+            <p className="text-xs truncate">
+              by{" "}
+              <Link to="#" className="font-semibold">
+                {product.author}
+              </Link>
+            </p>
           </div>
-        );
-      })}
+          <div className="flex flex-wrap justify-between items-center text-sm gap-4">
+            <div className="tracking-tight line-clamp-2 min-h-[3rem]">
+              <p className="text-lg font-bold">
+                Rp. {""}
+                {product.price.toLocaleString("id-ID")}
+              </p>
+              <p className="text-xs">{product.sale} Terjual</p>
+            </div>
+            <Button
+              href={product.url}
+              variant="primary"
+              text="Lihat Demo"
+              className="w-full lg:w-fit"
+              icon={Video}
+              iconClassName="w-4 h-4"
+            />
+          </div>
+        </div>
+      ))}
     </>
   );
 };
