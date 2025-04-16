3945daf7e6de35ab3332b1b33a5489cf437c2522
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-16 20:01:51 +0700

    refactor[Nexasite.type.ts]: added interface creatorprops

--- src/types/Nexasite.type.ts
+++ src/types/Nexasite.type.ts
@@ -1,12 +1,23 @@
+import { LucideIcon } from "lucide-react";
+
 export interface InstructionItem {
   title: string;
   commands: string; // Bisa juga dibuat string | string[] jika ingin fleksibel
 }
 
+export interface CreatorProps {
+  id: number;
+  name: string;
+  position: string;
+  image: string;
+  description: string;
+  socialMedia: string[];
+  iconMedia: LucideIcon[];
+}
+
 export interface NexasitesProps {
   id: number;
   name: string;
-  author: string;
   category: string;
   image: string;
   description: string[];
@@ -14,6 +25,8 @@ export interface NexasitesProps {
   url: string;
   price: number;
   sale: number;
+  author: string;
+  creator?: CreatorProps;
   nameTechStack: string[];
   imageTechStack: string[];
   linkTechStack: string[];
