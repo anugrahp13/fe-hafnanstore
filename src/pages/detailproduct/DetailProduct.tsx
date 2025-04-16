677db11fce09018645777ceba0ba48842e79bc14
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-16 20:03:25 +0700

    refactor[DetailProduct.tsx]: import PriceProduct, CreatorProduct, and useNexasiteStore.

--- src/pages/detailproduct/DetailProduct.tsx
+++ src/pages/detailproduct/DetailProduct.tsx
@@ -1,9 +1,8 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { NexasitesProps } from "../../types/Nexasite.type";
 import useBreadcrumbStore from "../../stores/useBreadcrumbStore";
 import { useEffect } from "react";
 import Breadcrumb from "../../components/elements/Breadcrumb";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { ShoppingCart, Star } from "lucide-react";
 import { FeatureProduct } from "./FeatureProduct";
 import { CompotibleProduct } from "./CompotibleProduct";
@@ -12,32 +11,37 @@ import { InstructionProduct } from "./InstructionProduct";
 import { ToolProduct } from "./ToolProduct";
 import { AboutProduct } from "./AboutProduct";
 import { LanguageProduct } from "./LanguageProduct";
+import { PriceProduct } from "./PriceProduct";
+import { CreatorProduct } from "./CreatorProduct";
+import { useNexasiteStore } from "../../stores/useDetailProductStore";
 
-interface DetailProductProps {
-  nexasites: NexasitesProps;
-}
-export const DetailProduct = ({ nexasites }: DetailProductProps) => {
+export const DetailProduct = () => {
+  const { productSlug } = useParams();
+  const { getProductBySlug } = useNexasiteStore();
   const { setBreadcrumbs } = useBreadcrumbStore();
+
+  // Dapatkan produk berdasarkan slug
+  const product = getProductBySlug(productSlug || "");
   useEffect(() => {
+    if (!product) return;
+
     window.scrollTo(0, 0);
-    setBreadcrumbs([
+    const newBreadcrumbs = [
       { label: "Home", path: "/" },
       { label: "Products", path: "/products" },
       { label: "Nexasite", path: "/products/nexasite" },
-      { label: nexasites.role, path: "" },
-      { label: nexasites.name, path: "" },
-    ]);
+      { label: product.name, path: "" },
+    ];
 
-    return () => {
-      // Reset breadcrumbs when component unmounts if needed
-      // Or you can manage this in the parent component
-    };
-  }, [nexasites, setBreadcrumbs]);
+    setBreadcrumbs(newBreadcrumbs);
+  }, [product, setBreadcrumbs]); // Pastikan product stabil
+
+  if (!product) return <div>Product not found</div>;
   return (
     <>
       <HelmetProvider>
         <Helmet>
-          <title>Portfolio Anugrah Prastyo</title>
+          <title>{product.name} | Portfolio Anugrah Prastyo</title>
         </Helmet>
       </HelmetProvider>
       <section className="my-32 dark:bg-dark">
@@ -48,16 +52,22 @@ export const DetailProduct = ({ nexasites }: DetailProductProps) => {
               {/* Breadcrumb */}
               <Breadcrumb className="hidden lg:flex" />
               <div className="flex flex-col gap-2">
-                <div className="text-4xl font-bold">{nexasites.name}</div>
+                <div className="text-4xl font-bold">{product.name}</div>
                 <div className="flex justify-between items-center text-base text-gray-600 gap-2">
                   <div className="flex justify-center items-center dark:text-white gap-4">
                     <p>
                       by{" "}
                       <Link
-                        to="#"
+                        to={
+                          product.creator
+                            ? `/creator/${product.creator.id}`
+                            : "#"
+                        }
                         className="text-primary dark:text-white dark:hover:underline font-semibold"
                       >
-                        {nexasites.author}
+                        {product.creator
+                          ? product.creator.name
+                          : product.author}
                       </Link>
                     </p>
                     <div className="flex items-center gap-1">
@@ -66,38 +76,34 @@ export const DetailProduct = ({ nexasites }: DetailProductProps) => {
                         fill="currentColor"
                       />
                       <span className="mx-1 font-bold">4.8</span>
-                      <span className="text-sm">(2 Ulasan)</span>
+                      <span className="text-sm">({product.sale} Ulasan)</span>
                     </div>
                   </div>
                   <p className="flex items-center text-dark dark:text-white gap-2">
                     <ShoppingCart className="w-5 h-5" />
-                    <span className="font-bold">{nexasites.sale} Sales</span>
+                    <span className="font-bold">{product.sale} Sales</span>
                   </p>
                 </div>
-                {/* <p className="text-2xl font-bold">
-                  Rp. {""}
-                  {nexasites.price.toLocaleString("id-ID")}
-                </p> */}
                 <div className="grid gap-4 mt-4">
                   <picture>
                     <img
-                      src={nexasites.image}
-                      alt={nexasites.name}
+                      src={product.image}
+                      alt={product.name}
                       data-size="auto"
                       className="rounded-xl w-full lazyload object-cover brightness-90 dark:brightness-100 lazyloaded"
                     />
                   </picture>
                   <div className="grid text-left leading-relaxed gap-2">
                     {/* Bagian About */}
-                    <AboutProduct nexasites={nexasites} />
+                    <AboutProduct nexasites={product} />
                     {/* Bagian Feature */}
-                    <FeatureProduct nexasites={nexasites} />
+                    <FeatureProduct nexasites={product} />
                     {/* Bagian Compotible */}
-                    <CompotibleProduct nexasites={nexasites} />
+                    <CompotibleProduct nexasites={product} />
                     {/* Bagian Review */}
-                    <ChooseProduct nexasites={nexasites} />
+                    <ChooseProduct nexasites={product} />
                     {/* Bagian Instructions */}
-                    <InstructionProduct nexasites={nexasites} />
+                    <InstructionProduct nexasites={product} />
 
                     <div>
                       Semoga dengan adanya template{" "}
@@ -112,11 +118,17 @@ export const DetailProduct = ({ nexasites }: DetailProductProps) => {
             </div>
             <div className="md:w-1/4">
               <div className="grid gap-8">
-                {/* Bagian Bahasa Pemrograman */}
-                <LanguageProduct nexasites={nexasites} />
+                {/* Bagian Harga */}
+                <PriceProduct nexasites={product} />
 
-                {/* Bagian Alat */}
-                <ToolProduct nexasites={nexasites} />
+                {/* Bagian Language Programming */}
+                <LanguageProduct nexasites={product} />
+
+                {/* Bagian Tool */}
+                <ToolProduct nexasites={product} />
+
+                {/* Bagian Creator */}
+                <CreatorProduct nexasites={product} />
               </div>
             </div>
           </div>
