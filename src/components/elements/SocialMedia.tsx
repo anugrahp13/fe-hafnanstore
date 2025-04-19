8f7d6cc0214a442f7d2ac896f7d28868cb214d85
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-19 22:14:20 +0700

    refactor[SocialMedia.tsx]: change property width and hight, and change const

--- src/components/elements/SocialMedia.tsx
+++ src/components/elements/SocialMedia.tsx
@@ -1,28 +1,27 @@
 import { useNavigate } from "react-router-dom";
 import { SocialMediaProps } from "../../types/SocialMedia.type";
 
-export const Sosmed: React.FC<SocialMediaProps> = ({
-    to,
-    icon: Icon = () => null,
-    iconClassName,
-  }) => {
-    const navigate = useNavigate();
-  
-    const handleClick = () => {
-      if (to.startsWith("http")) {
-        window.open(to, "_blank");
-      } else {
-        navigate(to);
-      }
-    };
-  
-    const defaultStyle =
-      "w-9 h-9 rounded-full flex justify-center items-center border border-slate-300 text-slate-300 hover:border-primary hover:text-white hover:bg-primary dark:border-white hover:dark:border-primary";
-  
-    return (
-      <button onClick={handleClick} className={defaultStyle}>
-        <Icon className={iconClassName} />
-      </button>
-    );
+export const SocialMedia: React.FC<SocialMediaProps> = ({
+  to,
+  icon: Icon = () => null,
+  iconClassName,
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (to.startsWith("http")) {
+      window.open(to, "_blank");
+    } else {
+      navigate(to);
+    }
   };
-  
+
+  const defaultStyle =
+    "w-10 h-10 rounded-full flex justify-center items-center border border-slate-300 text-slate-300 hover:border-primary hover:text-white hover:bg-primary dark:border-white hover:dark:border-primary";
+
+  return (
+    <button onClick={handleClick} className={defaultStyle}>
+      <Icon className={iconClassName} />
+    </button>
+  );
+};
