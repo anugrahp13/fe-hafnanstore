1f80aa69056019f1b1331b8496a6357d472e16cd
anugrahp13 <anugrahprastyo13@gmail.com>
2025-05-09 22:06:44 +0700

    refactor[HafnanMart.type.ts]: remove price. logo, detail, and specification

--- src/types/HafnanMart.type.ts
+++ src/types/HafnanMart.type.ts
@@ -1,22 +1,13 @@
+import { IconType } from "react-icons";
+
 export interface HafnanMartsProps {
   id: number;
   name: string;
-  price: string;
   category: string;
   image: string;
   marketplaces: {
     name: string;
-    logo: string;
+    icon?: IconType;
     url: string;
-  }[];
-  detail: {
-    condition: string;
-    minOrder: string;
-    storefront: string;
-    description: string;
-  }[];
-  specification: {
-    storage: string;
-    timeStorage: string;
   }[];
 }
