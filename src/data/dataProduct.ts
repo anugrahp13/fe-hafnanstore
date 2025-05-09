3e1437fe41fc2203c2f80575cc5331207706df59
anugrahp13 <anugrahprastyo13@gmail.com>
2025-05-09 22:03:33 +0700

    refactor[dataProduct.ts]: changed format webp for image

--- src/data/dataProduct.ts
+++ src/data/dataProduct.ts
@@ -2,17 +2,17 @@ const dataProduct = [
   {
     id: 1,
     name: "Hafnan Mart",
-    image: "image/products/hafnanmart/hafnanmart.png",
+    image: "image/products/hafnanmart/hafnanmart.webp",
   },
   {
     id: 2,
     name: "Hafnan Digital",
-    image: "image/products/hafnandigital/hafnandigital.png",
+    image: "image/products/hafnandigital/hafnandigital.webp",
   },
   {
     id: 3,
     name: "Nexasite",
-    image: "image/products/nexasite/nexasite.png",
+    image: "image/products/nexasite/nexasite.webp",
   },
 ];
 
