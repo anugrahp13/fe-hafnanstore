2925e7919100526a99889cefe85f47c6757a58a7
anugrahp13 <anugrahprastyo13@gmail.com>
2025-05-09 22:01:45 +0700

    refactor[dataHafnanDigitals]: changed format webp for image

--- src/data/dataHafnanDigital.ts
+++ src/data/dataHafnanDigital.ts
@@ -3,77 +3,77 @@ const dataHafnanDigital = [
     id: 1,
     name: "VIDEO AFFILIATE PRODUK TERLARIS",
     image:
-      "/image/products/hafnandigital/VIDEO AFFILIATE PRODUK TERLARIS.png",
+      "/image/products/hafnandigital/VIDEO AFFILIATE PRODUK TERLARIS.webp",
     url: "https://lynk.id/hafnandigital/rxq7791",
   },
   {
     id: 2,
     name: "Digital Satu Persen  Akademi Creator NR HOUSE",
     image:
-      "/image/products/hafnandigital/Digital Satu Persen  Akademi Creator NR HOUSE.png",
+      "/image/products/hafnandigital/Digital Satu Persen Akademi Creator NR HOUSE.webp",
     url: "https://lynk.id/hafnandigital/mn11zdB",
   },
   {
     id: 3,
     name: "MAHIR DIGITAL - Platform Belajar Digital Marketing Dari GAPTEK Sampai Bisa PRAKTEK DIGITAL MARKETING",
     image:
-      "/image/products/hafnandigital/MAHIR DIGITAL - Platform Belajar Digital Marketing Dari GAPTEK Sampai Bisa PRAKTEK DIGITAL MARKETING.png",
+      "/image/products/hafnandigital/MAHIR DIGITAL - Platform Belajar Digital Marketing Dari GAPTEK Sampai Bisa PRAKTEK DIGITAL MARKETING.webp",
     url: "https://lynk.id/hafnandigital/pYX5WRP",
   },
   {
     id: 4,
     name: "BATCH 10-Belajar Bareng Shopee Live by Wulanmiulan",
     image:
-      "/image/products/hafnandigital/BATCH 10-Belajar Bareng Shopee Live by Wulanmiulan.png",
+      "/image/products/hafnandigital/BATCH 10-Belajar Bareng Shopee Live by Wulanmiulan.webp",
     url: "https://lynk.id/hafnandigital/28bE2YN",
   },
   {
     id: 5,
     name: "Organic Brand Blueprint",
     image:
-      "/image/products/hafnandigital/Organic Brand Blueprint.png",
+      "/image/products/hafnandigital/Organic Brand Blueprint.webp",
     url: "https://lynk.id/hafnandigital/eB4zDzV",
   },
   {
     id: 6,
     name: "Ebook Cuan Dari Rumah",
     image:
-      "/image/products/hafnandigital/Ebook Cuan Dari Rumah.png",
+      "/image/products/hafnandigital/Ebook Cuan Dari Rumah.webp",
     url: "https://lynk.id/hafnandigital/RKeeraV",
   },
   {
     id: 7,
     name: "100 JUTA PERTAMA DARI AI: Belajar Lengkap AI Tools Penghasil CUAN with Anjas Maradita Course",
     image:
-      "/image/products/hafnandigital/100 JUTA PERTAMA DARI AI.png",
+      "/image/products/hafnandigital/100 JUTA PERTAMA DARI AI.webp",
     url: "https://lynk.id/hafnandigital/NRr6Z5W",
   },
   {
     id: 8,
     name: "Rahasia Strategi Kohen menghasilkan ratusan juta dari tiktok",
     image:
-      "/image/products/hafnandigital/Rahasia Strategi Kohen menghasilkan ratusan juta dari tiktok.png",
+      "/image/products/hafnandigital/Rahasia Strategi Kohen menghasilkan ratusan juta dari tiktok.webp",
     url: "https://lynk.id/hafnandigital/Vwj8lzW",
   },
   {
     id: 9,
     name: "200+ POWER POINT (PPT) TEMPLATES [Hak Jual Kembali - Keuntungan 100%]",
     image:
-      "/image/products/hafnandigital/200+ POWER POINT (PPT) TEMPLATES.png",
+      "/image/products/hafnandigital/200 POWER POINT (PPT) TEMPLATES.webp",
     url: "https://lynk.id/hafnandigital/0M4lGbY",
   },
   {
     id: 10,
     name: "WOW!! RIBUAN VIDEO AFFILIATE PRODUK TERLARIS SIAP PAKAI!! (HAK MENJUAL KEMBALI PROFIT 100%!!) by Eritaqueen",
     image:
-      "/image/products/hafnandigital/WOW!! RIBUAN VIDEO AFFILIATE PRODUK TERLARIS SIAP PAKAI!!.png",
+      "/image/products/hafnandigital/WOW!! RIBUAN VIDEO AFFILIATE PRODUK TERLARIS SIAP PAKAI!!.webp",
     url: "https://lynk.id/hafnandigital/0AlnpL5",
   },
   {
     id: 11,
     name: "Canva Template - PLR/Reseller by @upekhalid]",
     image:
-      "/image/products/hafnandigital/Canva Template - PLR(Reseller).png",
+      "/image/products/hafnandigital/Canva Template - PLR(Reseller).webp",
     url: "https://lynk.id/hafnandigital/6gOy6Be",
   },
 ];
