dc2c62255736dc24000d18cc0bef026cd6bdf53d
anugrahp13 <anugrahprastyo13@gmail.com>
2025-05-09 22:03:16 +0700

    refactor[dataNexasite.ts]: remove url, paymentLink, and downloadUrl

--- src/data/dataNexasite.ts
+++ src/data/dataNexasite.ts
@@ -3,30 +3,23 @@ const dataNexasite = [
     id: 1,
     name: "Simple Portfolio",
     category: "Landing Page",
-    image: "/image/products/nexasite/simple-portfolio.png",
+    image: "/image/products/nexasite/simple-portfolio.webp",
     description: [
       "Merupakan solusi sempurna untuk profesional portfolio yang ingin menampilkan karya dengan cara yang stylish, simple, namun informatif, meningkatkan personal branding sekaligus menjadi alat marketing yang efektif.",
       "Template ini dirancang untuk developer, desainer, dan profesional kreatif yang ingin tampil clean, modren, dan profesional. Dibangun dengan desain minimalis namun powerful, template ini membantu Anda membuat kesan pertama yang kuat di mata klien atau recruiter.",
     ],
     role: "Frontend",
-    url: "https://lynk.id/hafnandigital/0M4lGbY",
+    url: "https://youtu.be/_r09H7vXYrI",
     price: 25000,
-    paymentLink: "https://app.sandbox.midtrans.com/payment-links/1745159998611",
-    downloadUrl:
-      "https://drive.google.com/file/d/1PtzwRiVCu_dv67DWRIllwzzOoYfzioIC/view?usp=sharing",
-    sale: 2,
+    link: "https://wa.me/6281220187038?text=Halo,%20saya%20ingin%20membeli%20produk%20Landing%20Page%20Simple%20Portfolio",
+    sale: 3,
     author: "Anugrah Prastyo",
-    nameTechStack: ["ReactJS", "TypeScript", "TailwindCSS"],
+    nameTechStack: ["ReactJS", "TailwindCSS"],
     imageTechStack: [
       "/image/techstack/react-js.svg",
-      "/image/techstack/typescript.svg",
       "/image/techstack/tailwindcss.svg",
     ],
-    linkTechStack: [
-      "https://react.dev/",
-      "https://www.typescriptlang.org/",
-      "https://tailwindcss.com/",
-    ],
+    linkTechStack: ["https://react.dev/", "https://tailwindcss.com/"],
     nameTool: ["Visual Studio Code", "Chrome"],
     imageTool: [
       "/image/techstack/visual-studio.svg",
@@ -44,132 +37,6 @@ const dataNexasite = [
         resolution: "Yes",
         browser: ["Chrome", "Firefox", "Opera", "Safari"],
         fileIncluded: ["CSS File", "JS File"],
-        layout: "Responsive",
-      },
-    ],
-    instructions: [
-      {
-        title:
-          "Kamu sudah download filenya? Silahkan lakukan install dependencies terlebih dahulu",
-        commands: "npm install",
-      },
-      {
-        title: "Jalankan proyek dengan perintah berikut",
-        commands: "npm run dev",
-      },
-    ],
-    features: [
-      {
-        boldText: "Desain Responsif",
-        normalText:
-          " – Tampil sempurna di semua perangkat (desktop, tablet, & mobile).",
-      },
-      {
-        boldText: "Layout Terstruktur",
-        normalText:
-          " – Informasi tersusun rapi, mulai dari profil, skill, proyek, hingga pengalaman kerja.",
-      },
-      {
-        boldText: "Visualisasi Skill yang Jelas",
-        normalText:
-          " – Progress bar untuk menampilkan tingkat keahlian (HTML, CSS, JavaScript, dll).",
-      },
-      {
-        boldText: "Portofolio Menarik",
-        normalText:
-          " – Display proyek dengan thumbnail dan progress penyelesaian.",
-      },
-      {
-        boldText: "Sertifikasi & Pengalaman",
-        normalText:
-          " – Bagian khusus untuk meningkatkan kredibilitas profesional Anda.",
-      },
-      {
-        boldText: "Mudah Dikustomisasi",
-        normalText:
-          " – Ganti teks, warna, dan gambar dengan cepat sesuai kebutuhan.",
-      },
-    ],
-    compatibles: [
-      {
-        boldText: "Freelancer",
-        normalText: " yang ingin memamerkan karya secara profesional.",
-      },
-      {
-        boldText: "Job Seeker",
-        normalText: " yang membutuhkan CV digital yang menarik.",
-      },
-      {
-        boldText: "Mahasiswa/New Grads",
-        normalText: " yang ingin membangun portofolio pertama.",
-      },
-      {
-        boldText: "Professional",
-        normalText: " yang ingin memperbarui tampilan online mereka.",
-      },
-    ],
-    chooses: [
-      {
-        boldText: "Tidak Ribet",
-        normalText: " – Cukup isi data Anda, langsung siap digunakan.",
-      },
-      {
-        boldText: "Fokus pada Konten",
-        normalText:
-          " – Desain yang tidak mengganggu, tetapi memperkuat pesan Anda.",
-      },
-      {
-        boldText: "Hemat Waktu",
-        normalText:
-          " – Tidak perlu mulai dari nol, tinggal personalisasi dan deploy.",
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Dashboard Administrator",
-    category: "Landing Page",
-    image: "/image/products/nexasite/simple-portfolio.png",
-    description: [
-      "Merupakan solusi sempurna untuk profesional portfolio yang ingin menampilkan karya dengan cara yang stylish, simple, namun informatif, meningkatkan personal branding sekaligus menjadi alat marketing yang efektif.",
-      "Template ini dirancang untuk developer, desainer, dan profesional kreatif yang ingin tampil clean, modren, dan profesional. Dibangun dengan desain minimalis namun powerful, template ini membantu Anda membuat kesan pertama yang kuat di mata klien atau recruiter.",
-    ],
-    role: "Frontend",
-    url: "https://lynk.id/hafnandigital/0M4lGbY",
-    price: 100000,
-    paymentLink: "https://app.sandbox.midtrans.com/payment-links/1745159998611",
-    downloadUrl:
-      "https://drive.google.com/file/d/1PtzwRiVCu_dv67DWRIllwzzOoYfzioIC/view?usp=sharing",
-    sale: 8,
-    author: "Anugrah Prastyo",
-    nameTechStack: ["ReactJS", "TypeScript", "TailwindCSS"],
-    imageTechStack: [
-      "/image/techstack/react-js.svg",
-      "/image/techstack/typescript.svg",
-      "/image/techstack/tailwindcss.svg",
-    ],
-    linkTechStack: [
-      "https://react.dev/",
-      "https://www.typescriptlang.org/",
-      "https://tailwindcss.com/",
-    ],
-    nameTool: ["Visual Studio Code", "Chrome"],
-    imageTool: [
-      "/image/techstack/visual-studio.svg",
-      "/image/techstack/chrome.svg",
-      "/image/techstack/tailwindcss.svg",
-    ],
-    linkTool: [
-      "https://code.visualstudio.com/download",
-      "https://www.google.com/chrome/",
-    ],
-    information: [
-      {
-        lastUpdate: "10 April 2025",
-        published: "19 April 2025",
-        resolution: "Yes",
-        browser: ["Chrome", "Firefox", "Opera", "Safari"],
-        fileIncluded: ["CSS File", "JavaScript File"],
         layout: "Responsive",
       },
     ],
